287211c661d40698ffe7f5aae0f340540bebb1fc
young <youngscientist.iysa@gmail.com>
2025-01-10 09:22:24 +0700

    new

---
+++ data/terms.jsx
@@ -0,0 +1,42 @@
+export const indonesiaTerms = (
+  <>
+    <h3>Peserta Online:</h3>
+    <ol>
+      <li>
+        Peserta yang tidak mengumpulkan dokumen persyaratan (extended abstract,
+        PowerPoint) setelah dua kali pengingat akan dianggap mengundurkan diri
+        secara otomatis.
+      </li>
+      <li>
+        Peserta wajib menggunakan judul dan extended abstract yang sesuai dengan
+        kategori yang diikuti.
+      </li>
+      <li>
+        Keputusan panitia terkait hal ini bersifat final dan tidak dapat
+        diganggu gugat.
+      </li>
+    </ol>
+  </>
+);
+
+export const internationalTerms = (
+  <>
+    <h3>Online Participants:</h3>
+    <ol>
+      <li>
+        Participants who do not submit the required documents (extended abstract,
+        PowerPoint) after two reminders will be considered to have resigned
+        automatically.
+      </li>
+      <li>
+        Participants must use the title and extended abstract in accordance with
+        the category they are participating in. (International participants must
+        use English for all required documents)
+      </li>
+      <li>
+        The committee&apos;s decision regarding this matter is final and cannot be
+        contested.
+      </li>
+    </ol>
+  </>
+);
