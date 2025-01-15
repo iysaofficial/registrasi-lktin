0d0e7303f7cdd8eec66509daa54de78fb8fc2ad5
unknown <youngscientist.iysa@gmail.com>
2025-01-15 09:37:32 +0700

    update loader 2 -k

--- pages/thankyoupage.jsx
+++ pages/thankyoupage.jsx
@@ -1,15 +1,18 @@
+import Link from "next/link";
 const Thankyou = () => {
   return (
     <>
-    <section className="thankyou">
-      <div>
-        <h1>
-          Thank you for registering! We appreciate your participation and look
-          forward to your involvement.
-        </h1>
-        <a href="/">Back to registration menu</a>
-      </div>
-    </section>
+      <section className="thankyou">
+        <div>
+          <h1>
+            Thank you for registering! We appreciate your participation and look
+            forward to your involvement.
+          </h1>
+          <Link href="/" legacyBehavior>
+            <a>Back to registration menu</a>
+          </Link>
+        </div>
+      </section>
     </>
   );
 };
